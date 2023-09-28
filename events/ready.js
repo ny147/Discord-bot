505c45795207f8f19615cdb4a0b0d8f754ed882b
Near <weerawich_won@student.mahidol.edu>
2023-09-28 23:20:18 +0700

    update clean-code2

--- events/ready.js
+++ events/ready.js
@@ -1,22 +1,32 @@
 const { Events } = require("discord.js");
 const embedded = require("../feature/embedded/embedded");
 const getTaskList = require("../feature/query/getTaskList");
-const Reminder = require("../feature/reminder");
+const reminder = require("../feature/reminder");
+
 module.exports = {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
 
-    // set reminder
-    getTaskList.then(async (TaskList) => {
-      await TaskList.map((e) => {
-        embedded.addFields({ name: e.Task, value: `${e.time}`, inline: true });
-        Reminder(e.Task, e.time, client);
-      });
-      client.channels.cache
-        .get("931062873088753684")
-        .send({ embeds: [embedded] });
-    });
+    getTaskList
+      .then(async (TaskList) => {
+        const setEmbeded = await TaskList.map((e) => {
+          embedded.addFields({
+            name: e.Task,
+            value: `${e.time}`,
+            inline: true,
+          });
+        }).then(() => {
+          client.channels.cache
+            .get("931062873088753684")
+            .send({ embeds: [embedded] });
+        });
+
+        const setReminder = await TaskList.map((e) => {
+          reminder(e.Task, e.time, client);
+        });
+      })
+      .catch((error) => console.error(error));
   },
 };
